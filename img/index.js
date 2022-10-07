fea9db28f0bfb4993311cd8f9833af8869051e84
Josh <103025999+kaye360@users.noreply.github.com>
2022-10-06 21:27:14 -0700

    Add files via upload

--- img/index.js
+++ img/index.js
@@ -1,16 +1,9 @@
 export const logo = require('./logo.svg').default
 
-export const gridIcon = require('./menu-icons/grid.png')
-export const tableIcon = require('./menu-icons/table.png')
-export const authorIcon = require('./menu-icons/author.png')
-export const titleIcon = require('./menu-icons/title.png')
-export const readIcon = require('./menu-icons/read.png')
-export const unreadIcon = require('./menu-icons/unread.png')
-export const allIcon = require('./menu-icons/all.png')
-export const newtoOldIcon = require('./menu-icons/newtoold.png')
-export const oldtonewIcon = require('./menu-icons/oldtonew.png')
-export const searchIcon = require('./menu-icons/search.png')
-export const addIcon = require('./menu-icons/add.png')
+export const menuAddIcon = require('./add.svg').default
+export const menuSettingsIcon = require('./settings.svg').default
+export const menuSearchIcon = require('./search.svg').default
+export const menuCloseIcon = require('./closeMenu.svg').default
 
 export const imgNotAvailable = require('./img-not-available.jpg')
 export const BookIconRead = require('./icon-read.png')
